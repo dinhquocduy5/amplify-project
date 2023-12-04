d08923d05f51473cb49aab0d7db6634ac56e245a
minhduc2611 <minhduc2611@gmail.com>
2023-12-04 21:41:43 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+
 import './App.css';
 
+Amplify.configure(amplifyconfig);
 function App() {
   return (
     <div className="App">
